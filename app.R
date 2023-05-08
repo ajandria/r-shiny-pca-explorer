f49bd38d27ae55f7045550dfade26e09740371d5
Adrian Janucik <adrian@MacBook-Pro.local>
2023-05-08 10:24:58 +0200

    add 3d

--- app.R
+++ app.R
@@ -24,7 +24,11 @@ ui <- fluidPage(
                    plotlyOutput("pca2d", height = "600px", width = "100%")
                  )
         ),
-        tabPanel("3D PCA Plot", plotlyOutput("pca3d"))
+        tabPanel("3D PCA Plot",
+                 jqui_resizable(
+                   plotlyOutput("pca3d", height = "600px", width = "100%")
+                 )
+        )
       )
     )
   )
@@ -40,6 +44,16 @@ server <- function(input, output, session) {
       tibble::column_to_rownames(var = 'Geneid') %>%
       t()
     prcomp(count_matrix, rank. = 2)
+  })
+  
+  
+  # Reactive function to load count matrix
+  normalized_pca_3d <- reactive({
+    req(input$norm_count_matrix)
+    count_matrix <- read_excel(input$norm_count_matrix$datapath) %>%
+      tibble::column_to_rownames(var = 'Geneid') %>%
+      t()
+    prcomp(count_matrix, rank. = 3)
   })
   
   # Reactive function to load metadata
@@ -75,7 +89,58 @@ server <- function(input, output, session) {
     return(normalized_pca2d_plot_all_i)
   })
   
+  # Render 3D PCA plot
+  output$pca3d <- renderPlotly({
+    normalized_pca_3d <- req(normalized_pca_3d())
+    metadata <- req(metadata())
+    
+    # Create label for PCA showing % of explained variance
+    normalized_pca_3d_sum <- summary(normalized_pca_3d)
+    normalized_pca_3d_sum_total <- sum(normalized_pca_3d_sum$importance[2, 1:3]) * 100
+    
+    # Create data frame with sample names and metadata
+    pca3d_info <- data.frame(sample = rownames(normalized_pca_3d$x)) %>%
+      left_join(metadata)
+    
+    View(pca3d_info)
+    print(rownames(normalized_pca_3d$x))
+    
+    Condition_3d <- pca3d_info$group
+    Sample_3d <- pca3d_info$sample
+    
+    # Create PCA plot
+    normalized_pca3d_plot_all_i <-
+      plot_ly(
+        data.frame(normalized_pca_3d$x),
+        text = ~ rownames(normalized_pca_3d$x),
+        x = ~ PC1,
+        y = ~ PC2,
+        z = ~ PC3,
+        color = ~ Condition_3d
+      ) %>%
+      add_markers() %>%
+      layout(scene = list(
+        xaxis = list(title = paste(
+          "PC1 -",
+          round(normalized_pca_3d_sum$importance[2, 1], 3)
+        )),
+        yaxis = list(title = paste(
+          "PC2 -",
+          round(normalized_pca_3d_sum$importance[2, 2], 3)
+        )),
+        zaxis = list(title = paste(
+          "PC3 -",
+          round(normalized_pca_3d_sum$importance[2, 3], 3)
+        ))
+      ))
+    
+    
+    return(normalized_pca3d_plot_all_i)
+  })
+  
 }
+
+
 
 # Run Shiny app
 shinyApp(ui = ui, server = server)
